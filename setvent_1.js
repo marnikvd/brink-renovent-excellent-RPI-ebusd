05566da5768bd13bfad7a6cb39d06a57fbe05cd9
marnikvd <marnikvandendaele@gmail.com>
2018-04-22 14:37:03 +0100

    Add files via upload

---
+++ setvent_1.js
@@ -0,0 +1,59 @@
+// node.js script voor instellen stand ventilatie mbv mqtt
+// https://blog.risingstack.com/getting-started-with-nodejs-and-mqtt/
+
+/*
+als daemon uitvoeren met deze commando's
+pi@kat5pi1:~/brink $ node setvent_1.js </dev/null >/dev/null 2>&1 & disown
+*/
+
+const mqtt = require('/usr/lib/node_modules/mqtt')
+const client = mqtt.connect('mqtt://192.168.0.92:1883')
+const { exec } = require('child_process')
+
+client.on('connect', () => {
+	client.subscribe('renovent')
+})
+
+client.on('message', (topic, message) => {
+	console.log('received message %s %s', topic, message)
+	switch (topic) {
+		case 'renovent':
+			return handleRenoventRequest(message)
+	}
+})
+
+function handleRenoventRequest (message) {
+	console.log('ventilatie %s', message)
+	exec('bash ventctl.sh ' + message, (err, stdout, stderr) => {
+		if (err) {
+			// node couldn't execute the command
+			return;
+		}
+
+		// the *entire* stdout and stderr (buffered)
+		console.log(`stdout: ${stdout}`);
+		console.log(`stderr: ${stderr}`);
+	});
+}
+
+//https://stackoverflow.com/questions/14031763/doing-a-cleanup-action-just-before-node-js-exits/14032965#14032965
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+    if (options.cleanup) console.log('clean');
+    if (err) console.log(err.stack);
+    if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+//catches uncaught exceptions
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
---
+++ ventctl.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# 2018/04/21 Marnik : first commit
+
+# Execute this command on the command line to get usage discription :
+#	bash ventctl.sh
+
+ARGS=1         # Script requires 1 argument.
+E_BADARGS=85   # Wrong number of arguments passed to script.
+E_UNKNOWN=86   # Unknown option.
+
+PIPE="vent_pipe"
+
+# Function vent_read() is a daemon function
+# It is called with : 
+#	vent_read &
+#	sleep 1
+#	disown
+# The "&" makes it run in the background.
+# It will get it's own process ID but the name will still be the name of this script!
+# Commands to this daemon are passed via the pipe $PIPE
+# The "sleep 1" is needed to establish the pipe correctly.
+# If the sleep command is ommited, the pipe will be a regular file instead!!!
+# The "disown" will make this function stay alive when this shell script terminates
+# When the daemon terminates it will close the pipe (trap on EXIT).
+# The proper way to terminate the daemon is by executing the command :
+#	bash ventctl.sh stop
+vent_read() {
+
+	trap "rm -f $PIPE" EXIT
+
+	if [[ ! -p $PIPE ]]; then
+		mkfifo $PIPE
+	fi
+
+	read msg <$PIPE # wait for first message
+
+	while :; do # infinite loop
+		if [[ "$msg" == 'stop' ]]; then
+			exit 0
+		fi
+		ebusctl w -c kwl FanSpeed $msg >/dev/null
+
+		# https://stackoverflow.com/questions/6448632/read-not-timing-out-when-reading-from-pipe-in-bash
+		if read -t 60 <>$PIPE ; then
+			msg="$REPLY"
+		fi
+	done
+
+}
+
+if [ $# -eq "$ARGS" ]; then # check the number of arguments
+	case $1 in
+		"Minimal"|"Reduced"|"Normal"|"Intensive")
+		SPEEDMODE=$1
+		exitcode=1
+		;;
+		"stop")
+		exitcode=0
+		;;
+		*)
+		exitcode=$E_UNKNOWN
+		;;
+	esac
+else
+	exitcode=$E_BADARGS
+fi
+
+if [ "$exitcode" -gt 1 ]; then
+	echo "Usage: bash "$(basename $0)" Minimal|Reduced|Normal|Intensive|stop"
+	exit $exitcode
+fi
+
+if [ "$exitcode" -eq 0 ]; then
+	if [[ ! -p $PIPE ]]; then
+		echo "pipe not open - unable to send stop message"
+		exit 1
+	fi
+	echo "stop" >$PIPE
+	exit 0
+fi
+
+if [[ ! -p $PIPE ]]; then
+	vent_read &
+	sleep 1
+	disown
+fi
+
+echo "$SPEEDMODE" >$PIPE
